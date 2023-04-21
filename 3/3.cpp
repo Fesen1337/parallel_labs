488857c691146e7df5830872993708cf7f32428d
Fesen1337 <andrei.fesenk@gmail.com>
2023-04-21 16:08:58 +0300

    lab3

--- 3/3.cpp
+++ 3/3.cpp
@@ -1,59 +1,90 @@
-#include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <malloc.h>
+#include <mpi.h>
 
-#define VEC_SIZE 15
+#define ROWS 4
+#define COLS 5
 
-int main (int argc, char* argv[]) {
-
-	int rank, value = 0;
-  int array[15];
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  for (int i = 0; i < VEC_SIZE; i++)
-    array[i] = rank;
-  
-  if (rank != 0) {
-    char buffer[1024];
-    char tmp_buf[1024];
-    snprintf(buffer, 1024, "Init data for process %d: \n", rank);
-    for(int i = 0; i < VEC_SIZE; i++) {
-      snprintf(tmp_buf, 1024, "%d ", array[i]);
-      strcat(buffer, tmp_buf);
+void swap(char* a, char* b) {
+  char tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+void sort(char* mass, size_t size) {
+  for (size_t i = 0; i < size; i++) {
+    for (size_t j = 0; j < size - 1 - i; j++) {
+      if (*(mass + j) > *(mass + j + 1))
+        swap(mass + j, mass + j + 1);
     }
-    strcat(buffer, "\n\n");
-    printf("%s", buffer);
-
-  }else {
-    char buffer[1024];
-    char tmp_buf[1024];
-    snprintf(buffer, 1024, "Init data for Main_process: \n");
-    for(int i = 0; i < VEC_SIZE; i++) {
-      snprintf(tmp_buf, 1024, "%d ", array[i]);
-      strcat(buffer, tmp_buf);
-    }
-    strcat(buffer, "\n\n");
-    printf("%s", buffer);
   }
-  MPI_Barrier(MPI_COMM_WORLD);
+}
 
-  double start = MPI_Wtime();
-  MPI_Bcast(array, VEC_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-  double end = MPI_Wtime();
-
-  if (rank != 0) {
-    char buffer[1024];
-    char tmp_buf[1024];
-    snprintf(buffer, 1024, "Recieved data for process %d: \n", rank);
-    for(int i = 0; i < VEC_SIZE; i++) {
-      snprintf(tmp_buf, 1024, "%d ", array[i]);
-      strcat(buffer, tmp_buf);
+int main(int argc, char* argv[])
+{
+  char vec[ROWS][COLS];
+  int numprocs, myid, using_proc_count, ost = 0;
+  char* sorted_buf;
+  int *send_counts, *shift;
+  srand(time(NULL));
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+  if (myid == 0) {
+    printf("Init matrix:\n");
+    for (size_t i = 0; i < ROWS; i++) {
+      for (size_t j = 0; j < COLS; j++) {
+        vec[i][j] = rand() % 20;
+        printf("%d ", vec[i][j]);
+      }
+      printf("\n");
     }
-    snprintf(tmp_buf, 1024, "\nTime=%lf\n\n", end-start);
-    strcat(buffer, tmp_buf);
-    printf("%s", buffer);
+    printf("\n\n");
+  }
+  if (ROWS >= numprocs) {
+    ost = ROWS % numprocs;
+    using_proc_count = numprocs;
+  } else {
+    using_proc_count = ROWS;
+  }
+
+  if (myid < using_proc_count) {
+    send_counts = (int*)calloc(numprocs, sizeof(int));
+    shift = (int*)malloc(numprocs * sizeof(int));
+    for (size_t i = 0; i < using_proc_count; i++) {
+      if (ROWS >= numprocs) {
+        send_counts[i] = ROWS / numprocs * COLS;
+      } else {
+        send_counts[i] = COLS;
+      }
+      if (i > 0 && ost > 0)
+        if (i <= ost) shift[i] = i * send_counts[i] + i * COLS;
+        else shift[i] = i * send_counts[i] + ost * COLS;
+      else shift[i] = i * send_counts[i];
+      if (i < ost) send_counts[i] += COLS;
+    }
+    sorted_buf = (char*)malloc(send_counts[myid] * sizeof(char));
+    MPI_Scatterv(vec, send_counts, shift,
+    MPI_CHAR, sorted_buf, send_counts[myid],
+    MPI_CHAR, 0, MPI_COMM_WORLD);
+    for (size_t i = 0; i < (*send_counts); i += COLS) {
+      sort(sorted_buf + i, COLS);
+    }
+    MPI_Gatherv(sorted_buf, send_counts[myid], MPI_CHAR, vec,
+        send_counts, shift, MPI_CHAR, 0, MPI_COMM_WORLD);
+    free(sorted_buf);
+    free(send_counts);
+    free(shift);
+  }
+  if (myid == 0) {
+    printf("Sorted matrix:\n");
+    for (size_t i = 0; i < ROWS; i++) {
+      for (size_t j = 0; j < COLS; j++)
+        printf("%d ", vec[i][j]);
+      printf("\n");
+    }
+    printf("\n");
   }
   MPI_Finalize();
-
-	return 0;
 }
